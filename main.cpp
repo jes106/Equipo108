a6882763380cfc21d876f7a415f81a02ae562a50
i02dipea <i02dipea@uco.es>
2021-12-06 12:38:30 +0100

    Cambios generales
    Cambios generales

--- main.cpp
+++ main.cpp
@@ -3,43 +3,46 @@
 #include <cstring>
 #include <iostream>
 #include "Funciones.h"
+#include "Clases.h"
 
 using namespace std;
 
 int main(){
 
-    int encontrado = 0;
+    bool encontrado = false;
     int opcion = 0;
-    Usuario usuario;
+    Users users;
     string nick, password;
+    Usuario cliente;
 
 
-    while(encontrado == 0){
-        while(encontrado == 0){
+
+    while(encontrado == false){
+        while(encontrado == false){
             //En primer lugar vamos a pedir al usario que se identifique, para posteriormente comprobar si un usuario es 
             //administrador1, administrador2 o cliente, con el fin de mostrar un menu personalizado a los privilegios que 
             // tiene casa usuario
             /* Para esto debemos implementar la funcion "identificar_usuario" ya que es como lo expresamos en los diagamas de secuencia*/
             cout << endl << endl;
 
-            identificar_usuario(&nick, &password);
+            cliente.identificar_usuario();
 
-            usuario.nick = nick;
-            usuario.password = password;
+            users.nick = nick;
+            users.password = password;
 
             //Una vez introducido el nikname y la contraseña tendremos que sabe si el usuario esta registrado en el sistema o no. En caso 
             //de que no exita se tendra que proceder a realizar el registro.
-            encontrado = comprueba_login(&usuario);
+            encontrado = cliente.comprueba_login();
         }
         cout << endl << endl;
         //Una vez que el usuario ha accedido al sistema se le mostrara un menu personalizado en funcion del tipo de usuario
-        if(usuario.tipo == 1 && encontrado == 1){ opcion = menuAdmin1(); }
-        else if(usuario.tipo == 2){ opcion = menuAdmin2(); }
-        else if(usuario.tipo == 3){ opcion = menuUsuario(); }
+        if(cliente.gettipo() == 1 && encontrado == 1){ opcion = menuAdmin1(); }
+        else if(cliente.gettipo() == 2){ opcion = menuAdmin2(); }
+        else if(cliente.gettipo() == 3){ opcion = menuUsuario(); }
         if(opcion>=0 && opcion<=8){
             switch (opcion){
             case 0:
-                encontrado=0;
+                encontrado = false;
                 break;
             case 1:
                 crearUsuario();
