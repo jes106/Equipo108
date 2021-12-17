3faf583b95f588cb1966d18444221c2933100163
Alvarogg02 <i02gugaa@uco.es>
2021-12-17 12:43:00 +0100

    Update EQ108_tests.cc
    Test para los valores devueltos por los menús.

--- EQ108_tests.cc
+++ EQ108_tests.cc
@@ -1,2 +1,13 @@
 #include "Clases.h"
 #include "gtest/gtest.h"
+
+TEST(menuAdmin1,Testsmenus){
+
+    EXPECT_GT(menuAdmin1(),-1);
+    EXPECT_GT(menuAdmin2(),-1);
+    EXPECT_GT(menuUsuario(),-1);
+
+    EXPECT_LT(menuAdmin1(),9);
+    EXPECT_LT(menuAdmin2(),9);
+    EXPECT_LT(menuUsuario(),9);
+}
