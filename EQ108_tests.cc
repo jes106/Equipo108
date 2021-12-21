09148ca4f1905f44fdacfec8b1226214d46fa6fc
Alvarogg02 <i02gugaa@uco.es>
2021-12-21 12:34:15 +0100

    Update EQ108_tests.cc

--- EQ108_tests.cc
+++ EQ108_tests.cc
@@ -9,7 +9,6 @@ TEST(menuAdminUsuarios,Testsmenus){
 TEST(AdministradorUser, CrearUsuario){
 	AdministradorUsu adminus;
 	EXPECT_EQ(true, adminus.creaUsuario());
-	
 }
 
 TEST(AdministradorUser, ModificarUser){
@@ -19,20 +18,36 @@ TEST(AdministradorUser, ModificarUser){
 	
 	std::cout<<"---------------------------------"<<endl<<"PRUEBE A ELIMINAR i02essej"<<endl<<"---------------------------------"<<endl;
 	EXPECT_EQ(true,adminus.actualizaUsuario());
-	
 }
 
 TEST(menuAdminMaquinas,Testsmenus){
-    EXPECT_GT(menuAdmin2(),3);
+    EXPECT_GT(menuAdmin2(),-1);
     EXPECT_LT(menuAdmin2(),7);    
 }
 
 TEST(menuUsuarios,Testsmenus){
-    EXPECT_GT(menuUsuario(),6);
+    EXPECT_GT(menuUsuario(),-1);
     EXPECT_LT(menuUsuario(),9);
 }
 
 TEST(creaReserva,TestReserva){
 	Reservas res;
 	EXPECT_EQ(true,res.creaReserva("manolo"));
+}
+
+TEST(creaMaquina,TestMaquina){
+	AdministradorMaq maq;
+	EXPECT_EQ(true,maq.creaMaquina());
+}
+
+TEST(actualizaMaquina,TestMaquinaAct){
+	AdministradorMaq maq;
+	EXPECT_EQ(true,maq.actualizaMaquina());
+}
+
+TEST(busca_maquina,TestBuscaMaquina){
+	int id;
+	cout<<"Los nº de identificacion válidos son: 001\t002\t003\t004"<<endl;
+	cin>>id;
+	EXPECT_EQ(true,busca_maquina(id));
 }
